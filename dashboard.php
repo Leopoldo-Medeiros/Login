d860caabbc8f39f7405993c0959e99cae3f9ab0a
Leopoldo Medeiros <32571375+Leopoldo-Medeiros@users.noreply.github.com>
2022-04-25 09:36:59 -0300

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,21 @@
+<?php 
+$page_title = "Dashboard";
+include('includes/header.php'); 
+include('includes/navbar.php'); 
+?>
+
+<div class="py-5">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+          <div class="card-header">
+              <h4>User Dashboard</h4>
+          </div>
+            <div class="card-body">
+                <h5>Access when you Logged In</h5>
+            </div>
+      </div>
+    </div>
+  </div>
+</div>
+<?php include('includes/footer.php'); ?>
